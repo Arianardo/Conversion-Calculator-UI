8c7bc620f1d8e2cdba733bde229fbdb86c285c18
Ryan Stephens <rdsfkb@gmail.com>
2019-04-09 22:29:34 -0500

    Implemented UI

---
+++ Conversion Calculator UI/ConverterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ConverterViewController.swift
+//  Conversion Calculator UI
+//
+//  Created by Ryan Stephens on 4/9/19.
+//  Copyright © 2019 Ryan Stephens. All rights reserved.
+//
+
+import UIKit
+
+class ConverterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
